ff879ffde3365d3972ecb139a37c6e6183c67b51
Rokas Milasevicius <rokas@milasevicius.com>
2019-11-08 15:01:04 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: false, // Disables node env referenced from @digitalazgroup/eslint-config-react
+    'react-native/react-native': true,
+  },
+  extends: [
+    'plugin:react-native/all',
+  ],
+  plugins: [
+    'react-native',
+  ],
+  rules: {
+    // React-native Plugin
+    // The following rules are made available via eslint-plugin-react-native
+    'react-native/no-raw-text': 0 // All strings in React Native should be wrapped with a Text component; (disabled because rule is broken)
+  },
+}
